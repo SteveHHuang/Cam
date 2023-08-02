e70c5cfbbf50c9ee13b02d6bbfd6148f39b13728
SteveH <109771862+SteveHHuang@users.noreply.github.com>
2023-08-02 21:02:00 +0100

    add new

--- PROJECT.py
+++ PROJECT.py
@@ -3,9 +3,9 @@
 M = [0,0,0,0,0,0,0,0]  #memory
 
 R = [0,0,0,0,0,0,0,0]  #register
-listOfInstructions =[(0, "C", 1),
-      (1, "C", 1),(0, "LD", 1),(0, "ST", 1),
-      (0, "+", 1, 2),(0, "+", 2, 3),(0, "+", 3, 4),(0, "+", 4, 5)]
+#listOfInstructions =[(0, "C", 1),
+#      (1, "C", 1),(0, "LD", 1),(0, "ST", 1),
+#      (0, "+", 1, 2),(0, "+", 2, 3),(0, "+", 3, 4),(0, "+", 4, 5),(0, "+", 5, 6),(0, "+", 6, 7)]
 # 0, "LD", 1
 # 0, "ST", 1
 #print(R[0],R[1],R[2],R[3])
@@ -19,9 +19,12 @@ def isInt(s):
         return False
 
 def evalListOfInstructions(listOfInstructions, R, M):
+  print("eval")
   for i in range(0, len(listOfInstructions)):
     instr = listOfInstructions[i]
     print(R)
+    print(M)
+    print(instr)
     if len(instr) == 3:
        op = instr[1]
        if op == "C":
@@ -44,20 +47,25 @@ def evalListOfInstructions(listOfInstructions, R, M):
          if op == "+":  
            R[addr3] = R[addr1] + R[addr2]
          if op == "-":
-           R[addr3] = R[addr1] + R[addr2]
+           R[addr3] = R[addr1] - R[addr2]
          if op == "*":
            R[addr3] = R[addr1] * R[addr2]
          if op == "/":
            R[addr3] = R[addr1] / R[addr2]
     # Code that evaluates instruction by instruction in listOfInsructions
   return R
-evalListOfInstructions(listOfInstructions, R, M)
-print(R)
+#evalListOfInstructions(listOfInstructions, R, M)
+#print(R)
 
-listOfInstructions.clear()
-str="""a = 1
-b = 0
-c = a"""
+listOfInstructions = []
+str="""a = 5
+b = 3
+c = a
+a = b + c
+a = b - c
+a = b * c
+a = b / c
+"""
 pythStr = str.split("\n")
 print(pythStr)
 addrFree = 0
@@ -96,15 +104,55 @@ for line in pythStr:
         addr2 = addrFree
         addrFree += 1
         dicVarAddr[ops[2]] = addr1
-      instr1 = (0, "LD", addr2)
+      instr1 = (addr2, "LD", 0)
       instr2 = (0, "ST", addr1)
       listOfInstructions.append(instr1)
       listOfInstructions.append(instr2) 
     pass 
     # map to constant
+  elif len(ops) == 5:
+   
+    if ops[0] in dicVarAddr:
+      addr1 = dicVarAddr[ops[0]]
+    else:
+        addr1 = addrFree
+        addrFree += 1
+        dicVarAddr[ops[0]] = addr1 
+    
+    if ops[2] in dicVarAddr:
+      addr2 = dicVarAddr[ops[2]]
+    else:
+        addr2 = addrFree
+        addrFree += 1
+        dicVarAddr[ops[2]] = addr1 
+    
+    if ops[4] in dicVarAddr:
+      addr3 = dicVarAddr[ops[4]]
+    else:
+        addr3 = addrFree
+        addrFree += 1
+        dicVarAddr[ops[4]] = addr1  
+    instr1 = (addr2, "LD", 0)
+    instr2 = (addr3, "LD", 1)
+    if ops[3] == "+":
+      instr3 = (0, "+", 1, 1)
+    elif ops[3] == "-":
+      instr3 = (0, "-", 1, 1)
+    elif ops[3] == "*":
+      instr3 = (0, "*", 1, 1)
+    elif ops[3] == "/":
+      instr3 = (0, "/", 1, 1)
+    instr4 = (1, "ST", addr1)
+    listOfInstructions.append(instr1)
+    listOfInstructions.append(instr2) 
+    listOfInstructions.append(instr3)
+    listOfInstructions.append(instr4)
+    
   else: 
     pass
   print(listOfInstructions)
+evalListOfInstructions(listOfInstructions, R, M)
+print("Memory is", M)
     # map to classical operation
 """
 for i in range()
